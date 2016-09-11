6fa66446dac2b66f20a808fbe7cf210ccf697674
Shubham Lal <mail.shubhamlal@gmail.com>
2016-09-12 03:58:24 +0530

    Add files via upload

---
+++ outliers/outlier_cleaner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    
+    cleaned_data = []
+    import numpy
+
+    error = abs(net_worths - predictions)
+    sorted_error = numpy.sort(error, axis = None)
+    
+    max_error = sorted_error[int(len(sorted_error)*0.9)]
+    
+    for i in range(0, len(error)):
+        if error[i] < max_error:
+            cleaned_data.append((ages[i], net_worths[i], error[i]))
+            
+    print len(cleaned_data)
+    
+    
+    return cleaned_data
+
+
